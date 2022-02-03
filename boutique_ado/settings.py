5187d05b6dd98f6fe37f8660aca873c8f2b5e51d
jakelashbrook <jakeashleylashbrook@gmail.com>
2022-02-03 19:45:16 +0000

    Added home app and templates

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -58,7 +59,9 @@ ROOT_URLCONF = 'boutique_ado.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
