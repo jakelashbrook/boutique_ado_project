e75eb08fa337937d84bcec9d10b9eeb8bde1b310
jakelashbrook <jakeashleylashbrook@gmail.com>
2022-02-05 16:25:13 +0000

    Customized Admin

--- products/models.py
+++ products/models.py
@@ -2,11 +2,13 @@ from django.db import models
 
 
 class Category(models.Model):
-    ''' Organises the Category Model '''
 
+    class Meta:
+        verbose_name_plural = 'Categories'
+        
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
- 
+
     def __str__(self):
         return self.name
 
@@ -15,8 +17,6 @@ class Category(models.Model):
 
 
 class Product(models.Model):
-    ''' Organises the Product Model '''
-
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
