ca3151bedf6127ff8e6ba5acd5d9b794d37b5332
jakelashbrook <jakeashleylashbrook@gmail.com>
2022-02-06 17:38:19 +0000

    started product template

--- products/admin.py
+++ products/admin.py
@@ -1,25 +1,28 @@
 from django.contrib import admin
-
-from .models import Category, Product
+from .models import Product, Category
 
 
 class ProductAdmin(admin.ModelAdmin):
+    ''' Orders the fields in the Product Admin '''
     list_display = (
         'sku',
         'name',
         'category',
         'price',
         'rating',
-        'image'
+        'image',
     )
 
-    ordering = ("sku",)
+    ordering = ('sku',) # to reverse stick a - infront of sku
+
 
 class CategoryAdmin(admin.ModelAdmin):
+    ''' Prioritises showing the friendly name in Category Admin '''
     list_display = (
         'friendly_name',
-        'name'
+        'name',
     )
+
 
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Category, CategoryAdmin)
