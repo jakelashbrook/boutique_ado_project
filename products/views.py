78ec56e561eb2083f2283a995b0c894bf9e737d4
jakelashbrook <jakeashleylashbrook@gmail.com>
2022-02-06 19:02:35 +0000

    category links and product queries working

--- products/views.py
+++ products/views.py
@@ -1,34 +1,44 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product
+from .models import Product, Category
 
 # Create your views here.
+
 def all_products(request):
-    ''' A view to show all products, sorting and searches '''
+    """ A view to show all products, including sorting and search queries """
+
     products = Product.objects.all()
     query = None
+    categories = None
 
     if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
                 messages.error(request, "You didn't enter any search criteria!")
                 return redirect(reverse('products'))
-            # tells django to search for word in the name OR the description of product
-            # the i infront of contains makes the case insensitive for the search query
+            
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
     context = {
         'products': products,
+        'search_term': query,
+        'current_categories': categories,
     }
 
     return render(request, 'products/products.html', context)
 
 
 def product_detail(request, product_id):
-    ''' A view to show individual product details '''
+    """ A view to show individual product details """
+
     product = get_object_or_404(Product, pk=product_id)
 
     context = {
