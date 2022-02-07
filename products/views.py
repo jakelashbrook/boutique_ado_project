860607af88881f425c9bd41ecbf9920c54f7f67f
jakelashbrook <jakeashleylashbrook@gmail.com>
2022-02-07 14:27:51 +0000

    Optional Sorting Dropdown Added

--- products/views.py
+++ products/views.py
@@ -15,26 +15,20 @@ def all_products(request):
     direction = None
 
     if request.GET:
-        # Check whether sort is in request.GET
         if 'sort' in request.GET:
-            # If it is we set it equal to both sort & sortkey
             sortkey = request.GET['sort']
             sort = sortkey
-            # we rename sortkey to lower_name in the event the user is searching by name
             if sortkey == 'name':
                 sortkey = 'lower_name'
-                # we annotate the current list of products with a new field
-                products = products.annotate(lower_name=Lower('name'))
-
+                products = products.annotate(lower_name=Lower('name'))                
+            if sortkey == 'category':
+                sortkey = 'category__name'
             if 'direction' in request.GET:
                 direction = request.GET['direction']
-                # we check whether the direction is descending to decide whether to reverse the order
                 if direction == 'desc':
                     sortkey = f'-{sortkey}'
-            # sort the products 
             products = products.order_by(sortkey)
-
-
+            
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
