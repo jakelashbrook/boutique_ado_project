b613eb4363cca4029cb10c49351c2592ca8fcbaa
jakelashbrook <jakeashleylashbrook@gmail.com>
2022-02-15 17:21:34 +0000

    Added forms, admin and signals to checkout app

--- checkout/models.py
+++ checkout/models.py
@@ -6,8 +6,8 @@ from django.conf import settings
 
 from products.models import Product
 
+
 class Order(models.Model):
-    ''' A model to handle the checking out process '''
     order_number = models.CharField(max_length=32, null=False, editable=False)
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
@@ -24,16 +24,16 @@ class Order(models.Model):
     grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
 
     def _generate_order_number(self):
-        '''
-        Generate a random 32 character string as a unique order number using uuid
-        '''
+        """
+        Generate a random, unique order number using UUID
+        """
         return uuid.uuid4().hex.upper()
-    
+
     def update_total(self):
-        '''
-        Update the total each time a line item is added,
+        """
+        Update grand total each time a line item is added,
         accounting for delivery costs.
-        '''
+        """
         self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum']
         if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
             self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
@@ -43,10 +43,10 @@ class Order(models.Model):
         self.save()
 
     def save(self, *args, **kwargs):
-        ''' 
+        """
         Override the original save method to set the order number
-        if it hasn't been set already
-        '''
+        if it hasn't been set already.
+        """
         if not self.order_number:
             self.order_number = self._generate_order_number()
         super().save(*args, **kwargs)
@@ -56,22 +56,19 @@ class Order(models.Model):
 
 
 class OrderLineItem(models.Model):
-    ''' 
-    Processing the final order products
-    '''
     order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
-    product = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
     product_size = models.CharField(max_length=2, null=True, blank=True) # XS, S, M, L, XL
     quantity = models.IntegerField(null=False, blank=False, default=0)
     lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
 
     def save(self, *args, **kwargs):
-        ''' 
-        Override the original save method to set the order number
-        if it hasn't been set already
-        '''
+        """
+        Override the original save method to set the lineitem total
+        and update the order total.
+        """
         self.lineitem_total = self.product.price * self.quantity
         super().save(*args, **kwargs)
-    
+
     def __str__(self):
         return f'SKU {self.product.sku} on order {self.order.order_number}'
