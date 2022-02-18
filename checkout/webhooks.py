bc15d1f4f85f97d081b089d512366f3bae062ddd
jakelashbrook <jakeashleylashbrook@gmail.com>
2022-02-18 13:14:42 +0000

    Added checkout form data caching in payment intent

--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -10,7 +10,7 @@ import stripe
 @require_POST
 @csrf_exempt
 def webhook(request):
-    ''' Listen for webhooks from Stripe '''
+    """Listen for webhooks from Stripe"""
     # Setup
     wh_secret = settings.STRIPE_WH_SECRET
     stripe.api_key = settings.STRIPE_SECRET_KEY
@@ -21,30 +21,30 @@ def webhook(request):
     event = None
 
     try:
-        event = stripe.webhook.construct_event(
-            payload, sig_header, wh_secret
+        event = stripe.Webhook.construct_event(
+        payload, sig_header, wh_secret
         )
     except ValueError as e:
         # Invalid payload
         return HttpResponse(status=400)
     except stripe.error.SignatureVerificationError as e:
-        # Invalid Signature
+        # Invalid signature
         return HttpResponse(status=400)
     except Exception as e:
         return HttpResponse(content=e, status=400)
-    
-    # Set up a webhook handler 
+
+    # Set up a webhook handler
     handler = StripeWH_Handler(request)
 
     # Map webhook events to relevant handler functions
-    event.map = {
+    event_map = {
         'payment_intent.succeeded': handler.handle_payment_intent_succeeded,
-        'payment_method.payment_failed': handler.handle_payment_intent_payment_failed,
+        'payment_intent.payment_failed': handler.handle_payment_intent_payment_failed,
     }
-    
-    # Get the webhook type from stripe
+
+    # Get the webhook type from Stripe
     event_type = event['type']
-    
+
     # If there's a handler for it, get it from the event map
     # Use the generic one by default
     event_handler = event_map.get(event_type, handler.handle_event)
